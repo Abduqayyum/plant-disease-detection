5dfce8250731f2114dac1e58449229f229e19873
abduqayum.rasulmuhamedov <abduqayum.rasulmuhamedov@ssd.uz>
2023-09-07 00:20:00 +0500

    done:

--- plants_disease/settings.py
+++ plants_disease/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-=_@iid(8yet%0*+yu9hl*=4f4vdemo7r&!m3d+eo_(mh6$53ra"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("IS_PRODUCTION", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    os.getenv("APP_HOST")
+]
 
 
 # Application definition

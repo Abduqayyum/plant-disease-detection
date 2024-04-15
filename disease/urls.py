6bbc1b7b18ffa720b75d15cfbc697b717f99c5db
Abduqayyum <97339137+Abduqayyum@users.noreply.github.com>
2024-04-15 17:39:48 +0500

    Update urls.py

--- disease/urls.py
+++ disease/urls.py
@@ -8,8 +8,6 @@ router.register('images', views.ImageModelVS, basename='images')
 
 urlpatterns = [
     path("predict/<int:pk>", views.PredictionView.as_view(), name="prediction-view"),
+    path("", views.index),
     path("", include(router.urls)),
-    # path("upload/", views.ImageView.as_view(), name="upload-view"),
-    # path("upload/", views.image_view, name="image-view"),
-    path("index/", views.index, name="home-view"),
 ]
